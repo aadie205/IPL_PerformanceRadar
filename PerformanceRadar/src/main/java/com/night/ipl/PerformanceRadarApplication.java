b227fbe2ac7d23d4e63ad17bb730b38bcd99c8ad
Aditya Yadav <yadavaditya205@gmail.com>
2023-01-19 22:17:45 +0530

    First Commit

---
+++ PerformanceRadar/src/main/java/com/night/ipl/PerformanceRadarApplication.java
@@ -0,0 +1,13 @@
+package com.night.ipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceRadarApplication.class, args);
+	}
+
+}
